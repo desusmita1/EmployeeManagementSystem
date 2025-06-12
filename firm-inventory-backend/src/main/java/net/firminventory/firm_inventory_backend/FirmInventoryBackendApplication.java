12cba294766fde7a2f0aa836e315dca49b57aa8c
Susmita De <desusmita001@gmail.com>
2025-06-12 11:00:28 -0500

    Initial commit

---
+++ firm-inventory-backend/src/main/java/net/firminventory/firm_inventory_backend/FirmInventoryBackendApplication.java
@@ -0,0 +1,13 @@
+package net.firminventory.firm_inventory_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmInventoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmInventoryBackendApplication.class, args);
+	}
+
+}
---
+++ firm-inventory-backend/src/test/java/net/firminventory/firm_inventory_backend/FirmInventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.firminventory.firm_inventory_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmInventoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
